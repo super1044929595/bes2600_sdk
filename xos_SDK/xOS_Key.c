2694821ea6d90f5fb690497b08e5d4ebecef19b2
super1044929595 <1044929595@qq.com>
2023-12-17 15:48:07 +0800

    添加按重定义

--- xos_SDK/xOS_Key.c
+++ xos_SDK/xOS_Key.c
@@ -1,120 +1,124 @@
 #include "xOS_Key.h"
 #include "stdint.h"
 
+typedef  bool (*xOS_KeyHandle_Fucntion)(uint8_t * *pdata,uint16_t len);
 
-////////////////////////////////////////////////////////////////////////////////////////
 //XOS Key_Configure type
-typedef struct{
+typedef struct {
     xOS_KEY_BoardId            key_value;
     xOS_KEY_FunctionId         key_function;
     xOS_KEY_HandleStatus       key_status;
+    xOS_KeyHandle_Fucntion     ke_handle;
 }xOS_KeyType_t;
 
+bool xOS_KeyHandle_Fucntion_Test(uint8_t** pdata, uint16_t len)
+{
+    printf("\r\n xOS_KeyHandle_Fucntion_Test--->");
+    return false;
+}
 
 
-
-xOS_KeyType_t xos_shortkeygroup[]={
+xOS_KeyType_t xos_shortkeygroup1[] = {
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_PLAYANDPAUSE,
-        .key_status=1<<xOS_KEY_HANDLE_MEDIUM_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_MEDIUM_ACTIVE),
+        xOS_KeyHandle_Fucntion_Test,
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE),
+        NULL
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_OUTING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_OUTING_ACTIVE),
+        NULL
     },
 };
 
-xOS_KeyType_t xos_doublekeygroup[]={
+xOS_KeyType_t xos_shortkeygroup2[] = {
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_PLAYANDPAUSE,
-        .key_status=1<<xOS_KEY_HANDLE_MEDIUM_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_MEDIUM_ACTIVE),
+        xOS_KeyHandle_Fucntion_Test,
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE),
+        NULL
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_OUTING_ACTIVE,
-    },
-};
-
-xOS_KeyType_t xos_threekeygroup[]={
-    {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_PLAYANDPAUSE,
-        .key_status=1<<xOS_KEY_HANDLE_MEDIUM_ACTIVE,
-    },
-    {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE,
-    },
-    {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_OUTING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_OUTING_ACTIVE),
+        NULL
     },
 };
 
-xOS_KeyType_t xos_longkeygroup[]={
+xOS_KeyType_t xos_shortkeygroup3[] = {
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_PLAYANDPAUSE,
-        .key_status=1<<xOS_KEY_HANDLE_MEDIUM_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_MEDIUM_ACTIVE),
+        xOS_KeyHandle_Fucntion_Test,
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE),
+        NULL
     },
     {
-        .key_value=xOS_KEY_Board_POWER,
-        .key_function=xOS_KEY_FUNCTION_CALLUP,
-        .key_status=1<<xOS_KEY_HANDLE_CALL_OUTING_ACTIVE,
+        xOS_KEY_Board_DOWN,
+        xOS_KEY_FUNCTION_CALLUP,
+        (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_OUTING_ACTIVE),
+        NULL
     },
 };
+
+ const xOS_KeyType_t * const xosOne_ShortKey_Map[] = {
+    xos_shortkeygroup1,
+    xos_shortkeygroup2,
+    xos_shortkeygroup3,
+};
+
 
 
 uint8_t xOS_Key_GroupHandle(uint8_t key_type, uint8_t value)
 {
 
-    switch(key_type){
+    switch (key_type) {
 
         case  XOS_KEY_SHORT_E:
-        //short key handle
-        for( uint8_t i=0;i<sizeof(xos_shortkeygroup);i++){
-            if( xos_shortkeygroup[i].key_status ) {
-
-
+            //short key handle
+            if (xosOne_ShortKey_Map[value]->ke_handle) {
+                // get from sdk method
+                if (xosOne_ShortKey_Map[value]->key_status) { //*watch
+                    xosOne_ShortKey_Map[value]->ke_handle(NULL, 0);
+                }
             }
+            break;
+
+            //double key
+            case XOS_KEY_DOUBLE_E:
+
+            break;
+
+            //three key handle
+            case XOS_KEY_THREE_E:
+
+            break;
+
+            case XOS_KEY_LONG_E:
+
+            break;
         }
-        break;
 
-        //double key
-        case XOS_KEY_DOUBLE_E:
-
-        break;
-
-        //three key handle
-        case XOS_KEY_THREE_E:
-
-        break;
-
-        case XOS_KEY_LONG_E:
-
-        break;
+        return 0;
     }
 
-    return 0;
-}
