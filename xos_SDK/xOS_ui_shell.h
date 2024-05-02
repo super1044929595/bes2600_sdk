cb91907afdd47599f87189abafdfae7cc244bdd7
super1044929595 <1044929595@qq.com>
2024-05-02 16:53:06 +0800

    Update xOS_ui_shell.h

--- xos_SDK/xOS_ui_shell.h
+++ xos_SDK/xOS_ui_shell.h
@@ -3,11 +3,13 @@
 #ifdef __plusplus
     extern "C"{
 #endif
-#include "xOS_typedef.h"
+
+#include "xos_typedef.h"
 #include "cmsis_os.h"
 #include "app_thread.h"
 #include "app_utils.h"
 #include "xOS_Timer.h"
+#include "xos_sdk_configure.h"
 
 typedef enum{
 XOS_UI_WEAR_MOD,
@@ -17,9 +19,10 @@ XOS_UI_MAX_MOD,
 }XOS_Shell_UI_Mod_E;
 
 #if 1
-#define offsetof(type,member)  ((size_t)&((type*)0)->member)
 
-#define container_of(ptr,type,member)    ((type*)((char*)(ptr)-offsetof(type,member)))
+
+//#define offsetof(type,member)  ((size_t)&((type*)0)->member)
+//#define container_of(ptr,type,member)    ((type*)((char*)(ptr)-offsetof(type,member)))
 
 #else
 
@@ -41,10 +44,11 @@ typedef struct{
 }xOS_UI_shell_Info_t;
 bool xos_ui_shell_init(void);
 bool xos_ui_shell_send(XOS_Shell_UI_Mod_E mod,uint8_t *pdata,uint16_t len);
-
 bool xos_ui_shell_register(XOS_Shell_UI_Mod_E xshell_mod,xos_ui_shell_handle_typedef xshell_app);
 
 #ifdef __plusplus
    }
 #endif
 #endif
+
+
