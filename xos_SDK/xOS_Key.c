3a6ba60c075cb2f109d2cdb3e43dfb40acd7169b
super1044929595 <1044929595@qq.com>
2024-05-10 13:54:57 +0800

    Update xOS_Key.c

--- xos_SDK/xOS_Key.c
+++ xos_SDK/xOS_Key.c
@@ -1,7 +1,5 @@
 #include "xOS_key.h"
-#include "stdint.h"
-#include "hal_trace.h"
-
+#include "xos_systeminfo.h"
 
 typedef  bool (*xOS_KeyHandle_Fucntion)(uint8_t * *pdata,uint16_t len);
 
@@ -29,13 +27,13 @@ xOS_KeyType_t xos_shortkeygroup_Left[] = {
     },
     {
         xOS_KEY_Board_DOWN,
-        xOS_KEY_FUNCTION_CALLUP,
+        xOS_KEY_FUNCTION_ANC,
         (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE),
         NULL
     },
     {
         xOS_KEY_Board_DOWN,
-        xOS_KEY_FUNCTION_CALLUP,
+        xOS_KEY_FUNCTION_PLAYANDPAUSE,
         (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_OUTING_ACTIVE),
         NULL
     },
@@ -73,13 +71,13 @@ xOS_KeyType_t xos_doublekeygroup_Left[] = {
     },
     {
         xOS_KEY_Board_DOWN,
-        xOS_KEY_FUNCTION_CALLUP,
+        xOS_KEY_FUNCTION_PLAYANDPAUSE,
         (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_INCOMING_ACTIVE),
         NULL
     },
     {
         xOS_KEY_Board_DOWN,
-        xOS_KEY_FUNCTION_CALLUP,
+        xOS_KEY_FUNCTION_PLAYANDPAUSE,
         (xOS_KEY_HandleStatus)(2 << xOS_KEY_HANDLE_CALL_OUTING_ACTIVE),
         NULL
     },
@@ -121,32 +119,59 @@ const xOS_KeyType_t * const xos_double_Key_Map[] = {
 	xos_doublekeygroup_Right,
 };
 
+bool xos_KeyStatusSet(uint8_t tws_side,uint8_t key_type,uint8_t index,xOS_KEY_HandleStatus key_status)
+{
+	uint8_t _key_short_size=(uint8_t)sizeof(xos_shortkeygroup_Left)/sizeof(xos_shortkeygroup_Left[0]);
+
+	if(key_type==XOS_KEY_SHORT_E){		
+		
+		if(index>=_key_short_size)	return true;
+
+		if(tws_side==XOS_SYS_CHANNEL_LEFT){
+			xos_shortkeygroup_Left[index].key_status=key_status;
+		}else{
+			xos_shortkeygroup_Right[index].key_status=key_status;
+		}
+		
+	}else if(key_type==XOS_KEY_DOUBLE_E){
+
+	}else if(key_type==XOS_KEY_THREE_E){
+
+	}else if(key_type==XOS_KEY_LONG_E){
+
+	}else{
+
+	}
+	return false;
+}
 
 
+// It will be call by the button when user press
 uint8_t xOS_Key_GroupHandle(uint8_t key_type, uint8_t value)
 {
+	uint8_t _key_short_size=(uint8_t)sizeof(xos_shortkeygroup_Left)/sizeof(xos_shortkeygroup_Left[0]);
 
+    TRACE(2,"xOS_Key_GroupHandle key_type:%d value:%d",key_type,value);
+	
     switch (key_type) {
 
         case  XOS_KEY_SHORT_E:
-            //short key handle
-            if (xos_One_Shot_Key_Map[value]->ke_handle) {
-                // get from sdk method
-                if (xos_One_Shot_Key_Map[value]->key_status) { //*watch
-                    xos_One_Shot_Key_Map[value]->ke_handle(NULL, 0);
-                }
-            }
+            //short key handle			
+			for(int i=0;i<_key_short_size;i++){
+				if((xos_One_Shot_Key_Map[key_type][i].key_status)&&(xos_One_Shot_Key_Map[key_type][i].ke_handle)){
+						xos_One_Shot_Key_Map[key_type][i].ke_handle(NULL,0);
+				}
+			}
             break;
 
             //double key
             case XOS_KEY_DOUBLE_E:
-				if (xos_One_Shot_Key_Map[value]->ke_handle) {
-				// get from sdk method
-					if (xos_One_Shot_Key_Map[value]->key_status) { //*watch
-						xos_One_Shot_Key_Map[value]->ke_handle(NULL, 0);
-					}
+			if (xos_One_Shot_Key_Map[value]->ke_handle) {
+			// get from sdk method
+				if (xos_One_Shot_Key_Map[value]->key_status) { //*watch
+					xos_One_Shot_Key_Map[value]->ke_handle(NULL, 0);
 				}
-
+			}
             break;
 
             //three key handle
