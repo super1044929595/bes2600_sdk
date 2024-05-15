043b41a1ce70a97225d46583645f3d62f8ae512c
jw <huajinwen@risuntek.com>
2024-05-15 08:29:38 +0800

    添加驱动模式

---
+++ xos_SDK/xos_bsp_wear.c
@@ -0,0 +1,78 @@
+#include "xos_bsp_wear.h"
+
+
+#define XOS_BSP_WEAR_DEBUG_ENABLE
+#ifdef  XOS_BSP_WEAR_DEBUG_ENABLE 
+#define xos_bsp_wear_debug(format,...)     TRACE(5,"[xos bsp wear %d] %s "format "\n",__LINE__,__func__,##__VA_ARGS__);
+#else
+#define xos_bsp_wear_debug(format,...) 
+#endif
+
+#define XOS_BSP_USER_NUM    5
+#define XOS_BSP_WEAR_ID     0x64
+
+static XOS_BSP_Wear_Driver_s xos_bsp_wear_drive_info={
+	.name		=  "xos bsp drive",
+	.dev_name 	= "xos GH9023",
+	.probe      = NULL，
+};
+
+
+int xos_bsp_wear_regester(uint32_t *pdata,uint16_t len)
+{
+	xos_bsp_wear_debug(" drive regester ");
+	if(xos_bsp_wear_readchipid()==XOS_BSP_WEAR_ID){
+		if(!xos_bsp_wear_drive_info.probe(pdata,len)){
+			xos_bsp_wear_debug("regester error!");
+		}
+	}
+	return 0;
+}
+
+int xos_bsp_wear_readchipid(void)
+{
+	int chip_id=0;
+	
+	return chip_id;
+}
+
+int xos_bsp_wear_power_on(uint32_t *pdata,uint16_t len)
+{
+	if(!xos_bsp_wear_drive_info.poweron(pdata,len)){
+		xos_bsp_wear_debug("power on error!");
+	}
+	return 0;
+}
+
+int xos_bsp_wear_power_down(uint32_t *pdata,uint16_t len)
+{
+	if(!xos_bsp_wear_drive_info.shutdown(pdata,len)){
+		xos_bsp_wear_debug("power down error!");
+	}
+	return 0;
+}
+
+int xos_bsp_wear_unregester(uint32_t *pdata,uint16_t len)
+{
+	return 0;
+}
+
+int xos_bsp_wear_send(uint32_t *pdata,uint16_t len)
+{
+	return 0;
+}
+int xos_bsp_wear_receive(uint32_t *pdata,uint16_t len)
+{
+	return 0;
+}
+
+int xos_bsp_wear_suspend(uint32_t *pdata,uint16_t len)
+{
+	return 0;
+}
+
+int xos_bsp_wear_resume(uint32_t *pdata,uint16_t len)
+{
+	return 0;
+}
+
---
+++ xos_SDK/xos_bsp_wear.h
@@ -0,0 +1,48 @@
+#ifndef __XOS_BSP_WEAR_H
+#define __XOS_BSP_WEAR_H
+
+#ifdef __cplusplus
+	extern "C"{
+#endif
+
+typedef struct{
+
+	const char *name;
+	const char *dev_name;
+
+	int (*match)(void);
+	int (*uevent)(void);
+	int (*probe)(uint32_t *pdata,uint16_t len);
+	int (*remove)(uint32_t *pdata,uint16_t len);
+
+	int (*readchipid)(void);
+	int (*poweron)(uint32_t *pdata,uint16_t len);
+	int (*shutdown)(uint32_t *pdata,uint16_t len);
+
+	int (*online)(uint32_t *pdata,uint16_t len);
+	int (*offline)(uint32_t *pdata,uint16_t len);
+
+	int (*suspend)(uint32_t *pdata,uint16_t len);
+	int (*resume)(uint32_t *pdata,uint16_t len);
+
+	int (*callback)(uint32_t *pdata,uint16_t len);
+
+}XOS_BSP_Wear_Driver_s;
+
+int xos_bsp_wear_regester(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_regester(void);
+int xos_bsp_wear_readchipid(void);
+int xos_bsp_wear_power_on(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_power_down(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_unregester(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_send(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_receive(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_suspend(uint32_t *pdata,uint16_t len);
+int xos_bsp_wear_resume(uint32_t *pdata,uint16_t len);
+
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif
